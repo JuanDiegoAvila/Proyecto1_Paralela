38e1ebc8513ba64df2fdae38bbe959318c40d76b
JuanDiegoAvila <juanavila20027@gmail.com>
2023-08-31 19:32:53 -0600

    Nuevos dibujos, caritas felices sobre el circulo
    Co-authored-by: Diego Franco <Franco-611@users.noreply.github.com>
    Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>

--- secuencial.c
+++ secuencial.c
@@ -1,6 +1,6 @@
-// Juan Diego Avila Sagastume - 20090
-// Diego Jose Franco -  20240
-// Manuel Alejandro Archila - 161250
+// Manuel Archila - 161250
+// Diego Franco - 20240
+// Juan Diego Avila - 20090
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,33 +19,13 @@ typedef struct {
     int radio;
 } Circle;
 
-bool circlesOverlap(Circle c1, Circle c2){
-    float distance = sqrt(pow(c2.x - c1.x, 2) + pow(c2.y - c1.y, 2));
-    if (distance < c1.radio + c2.radio) return true;
-    return false;
-}
-
-SDL_Color mixColors(SDL_Color c1, SDL_Color c2) {
-    return (SDL_Color){
-        (c1.r + c2.r) / 2,
-        (c1.g + c2.g) / 2,
-        (c1.b + c2.b) / 2,
-        (c1.a + c2.a) / 2
-    };
-}
-
 void moveCircle(Circle *circle) {
     circle->x += circle->dx;
     circle->y += circle->dy;
 
-    if (circle->x - circle->radio <= 0 || circle->x + circle->radio >= WIDTH || circle->y - circle->radio <= 0 || circle->y + circle->radio >= HEIGHT) {
-        circle->color = (SDL_Color){rand() % 256, rand() % 256, rand() % 256, 255};
-    }
-
     if (circle->x - circle->radio <= 0 || circle->x + circle->radio >= WIDTH) circle->dx = -circle->dx;
     if (circle->y - circle->radio <= 0 || circle->y + circle->radio >= HEIGHT) circle->dy = -circle->dy;
 }
-
 
 void drawCircle(SDL_Renderer* renderer, Circle circle) {
     int x_center = circle.x;
@@ -63,7 +43,38 @@ void drawCircle(SDL_Renderer* renderer, Circle circle) {
     }
 }
 
+void fillCircle(SDL_Renderer* renderer, int x_center, int y_center, int r) {
+    for (int w = 0; w < r * 2; w++) {
+        for (int h = 0; h < r * 2; h++) {
+            int dx = r - w;
+            int dy = r - h;
+            if ((dx * dx + dy * dy) <= (r * r)) {
+                SDL_RenderDrawPoint(renderer, x_center + dx, y_center + dy);
+            }
+        }
+    }
+}
 
+void drawSmileyFace(SDL_Renderer* renderer, Circle circle) {
+    drawCircle(renderer, circle);
+
+    int eyeRadius = circle.radio / 8;
+    int eyeX1 = circle.x - circle.radio / 3;
+    int eyeY1 = circle.y - circle.radio / 3;
+    int eyeX2 = circle.x + circle.radio / 3;
+    int eyeY2 = circle.y - circle.radio / 3;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    fillCircle(renderer, eyeX1, eyeY1, eyeRadius);
+    fillCircle(renderer, eyeX2, eyeY2, eyeRadius);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = -circle.radio / 3; i < circle.radio / 3; i++) {
+        int x = circle.x + i;
+        int y = circle.y + circle.radio / 4 + sqrt(pow(circle.radio / 3, 2) - pow(i, 2)) / 2;
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -72,13 +83,8 @@ int main(int argc, char* argv[]) {
     }
 
     int n = atoi(argv[1]);
-    if (n <= 0) {
-        printf("Please provide a positive integer for number of circles.\n");
-        return 1;
-    }
 
     SDL_Init(SDL_INIT_VIDEO);
-
     SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
@@ -95,12 +101,8 @@ int main(int argc, char* argv[]) {
     }
 
     bool running = true;
-    Uint32 startTime;
-    int frameCount = 0;
 
     while (running) {
-        startTime = SDL_GetTicks();
-
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) running = false;
@@ -109,31 +111,13 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if (circlesOverlap(circles[i], circles[j])) {
-                    circles[i].color = (SDL_Color){rand() % 256, rand() % 256, rand() % 256, 255};
-                    circles[j].color = (SDL_Color){rand() % 256, rand() % 256, rand() % 256, 255};
-                }
-            }
-        }
-
         for (int i = 0; i < n; i++) {
             SDL_SetRenderDrawColor(renderer, circles[i].color.r, circles[i].color.g, circles[i].color.b, circles[i].color.a);
-            drawCircle(renderer, circles[i]);
+            drawSmileyFace(renderer, circles[i]);  // Cambio aquí
             moveCircle(&circles[i]);
         }
 
         SDL_RenderPresent(renderer);
-        frameCount++;
-
-        if (SDL_GetTicks() - startTime < 1000 / 30) {
-            SDL_Delay(1000 / 30 - (SDL_GetTicks() - startTime));
-        }
-
-        if (frameCount % 30 == 0) {
-            printf("FPS: %f\n", 30.0 * 1000 / (SDL_GetTicks() - startTime + 1));
-        }
     }
 
     SDL_DestroyRenderer(renderer);
@@ -142,4 +126,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
