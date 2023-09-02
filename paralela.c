f326c89949a67c1653fd7d7116f2f5061da6e040
Manuel-Archila <alejandroarchila15@gmail.com>
2023-09-01 20:56:05 -0600

    Paralelizado
    Co-authored-by: Diego Franco <Franco-611@users.noreply.github.com>
    Co-authored-by: JuanDiegoAvila <JuanDiegoAvila@users.noreply.github.com>

--- paralela.c
+++ paralela.c
@@ -1,6 +1,6 @@
-// Juan Diego Avila Sagastume - 20090
-// Diego Jose Franco -  20240
-// Manuel Alejandro Archila - 161250
+// Manuel Archila - 161250
+// Diego Franco - 20240 
+// Juan Diego Avila - 20090
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,6 @@
 #include <stdbool.h>
 #include <math.h>
 #include <time.h>
-#include <omp.h>
 
 #define WIDTH 640
 #define HEIGHT 480
@@ -18,48 +17,88 @@ typedef struct {
     float dx, dy;
     SDL_Color color;
     int radio;
-    bool relleno;
 } Circle;
 
 void moveCircle(Circle *circle) {
     circle->x += circle->dx;
     circle->y += circle->dy;
 
-    if (circle->x <= 0 || circle->x >= WIDTH) circle->dx = -circle->dx;
-    if (circle->y <= 0 || circle->y >= HEIGHT) circle->dy = -circle->dy;
+    if (circle->x - circle->radio <= 0 || circle->x + circle->radio >= WIDTH) circle->dx = -circle->dx;
+    if (circle->y - circle->radio <= 0 || circle->y + circle->radio >= HEIGHT) circle->dy = -circle->dy;
 }
 
-void drawCircle(SDL_Renderer* renderer, int x_center, int y_center, int r) {
-    
-    for (int w = 0; w < r * 2; w++) {
-        for (int h = 0; h < r * 2; h++) {
-            int dx = r - w; 
-            int dy = r - h; 
-            if ((dx * dx + dy * dy) <= (r * r)) {
+// Comprobar si dos círculos se superponen
+bool checkOverlap(Circle c1, Circle c2) {
+    float dx = c1.x - c2.x;
+    float dy = c1.y - c2.y;
+    float distance = sqrt(dx * dx + dy * dy);
+    return distance < (c1.radio + c2.radio);
+}
+
+void drawCircle(SDL_Renderer* renderer, Circle circle) {
+    int x_center = circle.x;
+    int y_center = circle.y;
+    int r = circle.radio;
+
+    for (int w = 0; w < r* 2; w++){
+        for (int h = 0; h < r*2; h++){
+            int dx = r - w; // horizontal offset
+            int dy = r - h; // vertical offset
+            if ((dx*dx + dy*dy) <= (r*r)) {
                 SDL_RenderDrawPoint(renderer, x_center + dx, y_center + dy);
             }
         }
     }
 }
 
+void fillCircle(SDL_Renderer* renderer, int x_center, int y_center, int r) {
+    for (int w = 0; w < r * 2; w++) {
+        for (int h = 0; h < r * 2; h++) {
+            int dx = r - w;
+            int dy = r - h;
+            if ((dx * dx + dy * dy) <= (r * r)) {
+                SDL_RenderDrawPoint(renderer, x_center + dx, y_center + dy);
+            }
+        }
+    }
+}
+// Funcion que dibuja una cara sonriente
+void drawSmileyFace(SDL_Renderer* renderer, Circle circle) {
+    drawCircle(renderer, circle);
+    
+    int eyeRadius = circle.radio / 8;
+    int eyeX1 = circle.x - circle.radio / 3;
+    int eyeY1 = circle.y - circle.radio / 3;
+    int eyeX2 = circle.x + circle.radio / 3;
+    int eyeY2 = circle.y - circle.radio / 3;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    fillCircle(renderer, eyeX1, eyeY1, eyeRadius);
+    fillCircle(renderer, eyeX2, eyeY2, eyeRadius);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = -circle.radio / 3; i < circle.radio / 3; i++) {
+        int x = circle.x + i;
+        int y = circle.y + circle.radio / 4 + sqrt(pow(circle.radio / 3, 2) - pow(i, 2)) / 2;
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        printf("Se debe ingresar el numero de circulos");
+        printf("Usage: %s <number_of_circles>\n", argv[0]);
         return 1;
     }
 
     int n = atoi(argv[1]);
 
     SDL_Init(SDL_INIT_VIDEO);
-
     SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     Circle circles[n];
     srand(time(NULL));
-
-    #pragma omp parallel for num_threads(n)
+    #pragma omp parallel for
     for (int i = 0; i < n; i++) {
         circles[i] = (Circle){
             rand() % WIDTH, rand() % HEIGHT,
@@ -84,26 +123,35 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+        
+        #pragma omp parallel for
+        for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (checkOverlap(circles[i], circles[j])) {
+                // Cambio de color y ajuste de direcciones para el rebote
+                circles[i].color = (SDL_Color){rand() % 256, rand() % 256, rand() % 256, 255};
+                circles[j].color = (SDL_Color){rand() % 256, rand() % 256, rand() % 256, 255};
 
-        #pragma omp parallel for num_threads(n)
-        for(int i = 0; i < n; i++) {
-            moveCircle(&circles[i]);
+                // Intercambiar direcciones (esto es un modelo muy simple de "rebote")
+                float temp_dx = circles[i].dx;
+                float temp_dy = circles[i].dy;
+                circles[i].dx = circles[j].dx;
+                circles[i].dy = circles[j].dy;
+                circles[j].dx = temp_dx;
+                circles[j].dy = temp_dy;
+            }
         }
+    }
 
         for (int i = 0; i < n; i++) {
             SDL_SetRenderDrawColor(renderer, circles[i].color.r, circles[i].color.g, circles[i].color.b, circles[i].color.a);
-            drawCircle(renderer, circles[i].x, circles[i].y, circles[i].radio);
+            drawSmileyFace(renderer, circles[i]);  // Dibujo de caras sonrientes
+            moveCircle(&circles[i]);
         }
-
-        
 
         SDL_RenderPresent(renderer);
+
         frameCount++;
-
-        if (SDL_GetTicks() - startTime < 1000 / 30) {
-            SDL_Delay(1000 / 30 - (SDL_GetTicks() - startTime));
-        }
-
         if (frameCount % 30 == 0) {
             printf("FPS: %f\n", 30.0 * 1000 / (SDL_GetTicks() - startTime + 1));
         }
@@ -115,4 +163,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
